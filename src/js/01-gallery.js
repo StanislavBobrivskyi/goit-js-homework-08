889f02194e7689930a8ca2952281d53cd21a3724
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-06-18 11:51:58 +0200

    task #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,39 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from '/node_modules/simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const createCard = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>`
+);
+
+const galleryStyle = document.querySelector('.gallery');
+
+galleryStyle.style.listStyle = 'none';
+
+galleryEl.insertAdjacentHTML('beforeend', createCard.join(''));
+
+galleryEl.addEventListener('click', onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
