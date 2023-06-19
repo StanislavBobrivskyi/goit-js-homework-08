0aa89c869ab57c606ac5dfdc3046c444d5e1f95a
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-06-19 20:55:55 +0200

    2 - task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,3 +3,22 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime)).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+}
